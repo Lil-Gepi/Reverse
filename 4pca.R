cdcbdf1cee32f1cc438a621e694016b125bb1127
Yiwen Chen <brandonchen2932@outlook.com>
2021-10-27 11:42:27 +0200

    add rep1314

--- 4pca.R
+++ 4pca.R
@@ -3,5 +3,6 @@
 library(poolSeq)
 F30_r11_r12 <- read.sync(file="~/RS/F30_all/result/F30_r11_r12_all_chr_mq20_bq20.sync",
                repl=rep(c(11,12), each=5), sub=rep(c(1,2,3,4,5),times=2), rising = FALSE)
+               
 F30_r11_r12_freq <- af(F30_r11_r12, repl=rep(c(11,12), each=5), sub=rep(c(1,2,3,4,5),times=2));
 F30_r11_r12_freq <- cbind(F30_r11_r12_freq, splitLocusID(rownames(F30_r11_r12_freq)))
