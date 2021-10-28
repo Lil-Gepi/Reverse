bb340a1d496cf5c22a8f1a3160bf96d9200beabf
Yiwen Chen <brandonchen2932@outlook.com>
2021-10-28 11:10:42 +0200

    fix 312

--- 1make_bam_n_move.sh
+++ 1make_bam_n_move.sh
@@ -3,7 +3,7 @@
 # It could be better if we intake a txt containing .cram file name and coordinate biological meaning, so that he bam file name make sense
 
 
-for cramfile in $(find /Volumes/Data/311/a/ /Volumes/Data/312/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+for cramfile in $(find /Volumes/Data/311/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
 do
   cramfilename=${cramfile%.*}
   bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
@@ -13,3 +13,13 @@ do
   samtools view -b /Volumes/Data/311/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
 done
 #samtools view -b /Volumes/Data/311/a/LB_311.TI02.cram -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/flight_test/data/F30/LB_311.TI02.bam
+
+for cramfile in $(find /Volumes/Data/312/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+do
+  cramfilename=${cramfile%.*}
+  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
+#  echo "$cramfilename"
+#  echo "$bamfilename"
+  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
+  samtools view -b /Volumes/Data/312/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
+done
