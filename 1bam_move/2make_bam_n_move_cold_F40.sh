c395230a497c793fa6c925393b23801a8dcc5123
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 17:59:29 +0100

    bam for all gens

--- 1bam_move/2make_bam_n_move_cold_F40.sh
+++ 1bam_move/2make_bam_n_move_cold_F40.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-
-# It could be better if we intake a txt containing .cram file name and coordinate biological meaning, so that he bam file name make sense
 
 
-for cramfile in $(find /Volumes/Data/311/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+
+for cramfile in $(find /Volumes/Data/185/*/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
 do
   cramfilename=${cramfile%.*}
-  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
+  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
 #  echo "$cramfilename"
 #  echo "$bamfilename"
-#  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
-  echo "samtools view -b /Volumes/Data/311/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam"
-done
-#samtools view -b /Volumes/Data/311/a/LB_311.TI02.cram -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/flight_test/data/F30/LB_311.TI02.bam
+  # if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
+  #   mkdir ~/RS/F30_all/data/${bamfilename}/
+  #   samtools view -b -q 20 -F 0x400 -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/311/a/${cramfile}
+  # fi
+rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
+samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa \
+-o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}
+done | parallel -j 10
