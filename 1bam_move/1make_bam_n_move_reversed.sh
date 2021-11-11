1985ef4e9d34f7da04814b17077f36d8dc9b98a7
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 20:18:39 +0100

    test parallel

--- 1bam_move/1make_bam_n_move_reversed.sh
+++ 1bam_move/1make_bam_n_move_reversed.sh
@@ -24,4 +24,4 @@ do
     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
     echo "samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa -o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}"
   done
-done | parallel -j 6
+done | parallel -j 10
