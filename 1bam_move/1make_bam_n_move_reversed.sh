7c63b83d9cd5f7eed90729150d6163cf6b887034
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 20:11:00 +0100

    test parallel

--- 1bam_move/1make_bam_n_move_reversed.sh
+++ 1bam_move/1make_bam_n_move_reversed.sh
@@ -8,14 +8,20 @@ do
   do
     cramfilename=${cramfile%.*}
     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
-  #  echo "$cramfilename"
-  #  echo "$bamfilename"
     # if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
     #   mkdir ~/RS/F30_all/data/${bamfilename}/
     #   samtools view -b -q 20 -F 0x400 -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/311/a/${cramfile}
     # fi
-  #rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
-  samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa \
-  -o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}
+  rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
   done
-done | parallel -j 10
+done
+#
+# for pool in 267 311 312 488 564 565 566
+# do
+#   for cramfile in $(find /Volumes/Data/${pool}/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+#   do
+#     cramfilename=${cramfile%.*}
+#     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
+#     echo "samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa -o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}"
+#   done
+# done | parallel -j 10
