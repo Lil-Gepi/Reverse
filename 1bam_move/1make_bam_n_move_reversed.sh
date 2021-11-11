629f08530916ee2ceffe5ca8d8288d88ea13e162
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 20:13:08 +0100

    test parallel

--- 1bam_move/1make_bam_n_move_reversed.sh
+++ 1bam_move/1make_bam_n_move_reversed.sh
@@ -8,11 +8,11 @@ do
   do
     cramfilename=${cramfile%.*}
     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
-    # if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
-    #   mkdir ~/RS/F30_all/data/${bamfilename}/
-    #   samtools view -b -q 20 -F 0x400 -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/311/a/${cramfile}
-    # fi
-  rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
+    if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
+      mkdir ~/RS/F30_all/data/${bamfilename}/
+    fi
+  # rm -r  ~/RS/data/${bamfilename}/
+  # mkdir ~/RS/data/${bamfilename}/
   done
 done
 #
