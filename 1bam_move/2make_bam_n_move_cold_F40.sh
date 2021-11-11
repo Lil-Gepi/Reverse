8c383dab3d46d65a6b9ae2e07380a4c03ca0852c
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 14:19:10 +0100

    modified names

---
+++ 1bam_move/2make_bam_n_move_cold_F40.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# It could be better if we intake a txt containing .cram file name and coordinate biological meaning, so that he bam file name make sense
+
+
+for cramfile in $(find /Volumes/Data/311/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+do
+  cramfilename=${cramfile%.*}
+  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
+#  echo "$cramfilename"
+#  echo "$bamfilename"
+  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
+  samtools view -b /Volumes/Data/311/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
+done
+#samtools view -b /Volumes/Data/311/a/LB_311.TI02.cram -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/flight_test/data/F30/LB_311.TI02.bam
+
+for cramfile in $(find /Volumes/Data/312/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+do
+  cramfilename=${cramfile%.*}
+  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
+#  echo "$cramfilename"
+#  echo "$bamfilename"
+  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
+  samtools view -b /Volumes/Data/312/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
+done
---
+++ 2bam_mpileup_sync/2mpileup_cold_F40.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# index the reference genome fasta
+samtools faidx ~/RS/F30_all/reference/dsimM252v1.2+microbiome.fa
+# do the job from bam to cram to sync for each subreplicate
+# bamfile are like F30r11sub1 etc.
+for bamfile in $(ls ~/RS/F30_all/data/)
+do
+  samtools index -b ~/RS/F30_all/data/${bamfile}/${bamfile}.bam
+done
+
+for chr in 2L 2R 3L 3R 4 X
+do
+  for bamfile in $(ls ~/RS/F30_all/data/)
+  do
+    cd ~/RS/F30_all/data/${bamfile}/
+    # split BAM into chromosomes of interest and index them,
+    # and we do the job on each chromosome and later concate them
+    # can also extract information for mitochondrial DNA which name is mtDNA_65039429 something
+    samtools view -b ~/RS/F30_all/data/${bamfile}/${bamfile}.bam ${chr} > \
+    ~/RS/F30_all/data/${bamfile}/${bamfile}_${chr}.bam
+    samtools index ~/RS/F30_all/data/${bamfile}/${bamfile}_${chr}.bam
+  done
+  samtools mpileup -B -q 20 -Q 0 -f ~/RS/F30_all/reference/dsimM252v1.2+microbiome.fa \
+  $(ls -R /Users/ychen/RS/F30_all/data/*/*_${chr}.bam) | \
+  java -jar ~/RS/F30_all/pipeline/popoolation2/mpileup2sync.jar --input /dev/stdin \
+  --output ~/RS/F30_all/result/F30_${chr}.sync --fastq-type sanger \
+  --min-qual 20 --threads 12
+
+done
+
+
+cat \
+~/RS/F30_all/result/F30_2L.sync ~/RS/F30_all/result/F30_2R.sync \
+~/RS/F30_all/result/F30_3L.sync ~/RS/F30_all/result/F30_3R.sync \
+~/RS/F30_all/result/F30_4.sync ~/RS/F30_all/result/F30_X.sync > \
+~/RS/F30_all/result/F30_all_chr_mq20_bq20.sync
+
+
+
+
+# old codes :
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam 2L > ../data/F30/Pool_311.F30r11sub1_2L.bam &&\
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam 2R > ../data/F30/Pool_311.F30r11sub1_2R.bam &&\
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam 3L > ../data/F30/Pool_311.F30r11sub1_3L.bam &&\
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam 3R > ../data/F30/Pool_311.F30r11sub1_3R.bam &&\
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam 4 > ../data/F30/Pool_311.F30r11sub1_4.bam &&\
+# samtools view -b /Users/ychen/RS/flight_test/data/F30/LB_311.TI02.bam X > ../data/F30/Pool_311.F30r11sub1_X.bam &&\
+#
+# samtools index ../data/F30/Pool_311.F30r11sub1_2L.bam &&\
+# samtools index ../data/F30/Pool_311.F30r11sub1_2R.bam &&\
+# samtools index ../data/F30/Pool_311.F30r11sub1_3L.bam &&\
+# samtools index ../data/F30/Pool_311.F30r11sub1_3R.bam &&\
+# samtools index ../data/F30/Pool_311.F30r11sub1_4.bam &&\
+# samtools index ../data/F30/Pool_311.F30r11sub1_X.bam &&\
+#
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_2L.bam > ../data/F30/Pool_311.F30r11sub1_2L.pileup &&\
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_2R.bam > ../data/F30/Pool_311.F30r11sub1_2R.pileup &&\
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_3L.bam > ../data/F30/Pool_311.F30r11sub1_3L.pileup &&\
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_3R.bam > ../data/F30/Pool_311.F30r11sub1_3R.pileup &&\
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_4.bam > ../data/F30/Pool_311.F30r11sub1_4.pileup &&\
+# samtools mpileup -B -q 20 -Q 0 -f ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa ../data/F30/Pool_311.F30r11sub1_X.bam > ../data/F30/Pool_311.F30r11sub1_X.pileup &&\
+#
+# # concate all the files into one
+# cd ../data/F30/
+# cat Pool_311.F30r11sub1_2L.pileup Pool_311.F30r11sub1_2R.pileup Pool_311.F30r11sub1_3L.pileup Pool_311.F30r11sub1_3R.pileup Pool_311.F30r11sub1_4.pileup Pool_311.F30r11sub1_X.pileup > Pool_311.F30r11sub1_allChr.pileup
