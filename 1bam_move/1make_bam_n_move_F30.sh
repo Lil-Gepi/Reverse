df41733f3ca74af3eefacbcd904c9bba7906d086
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-11 17:19:22 +0100

    bam for all gens

--- 1bam_move/1make_bam_n_move_F30.sh
+++ 1bam_move/1make_bam_n_move_F30.sh
@@ -2,24 +2,20 @@
 
 # It could be better if we intake a txt containing .cram file name and coordinate biological meaning, so that he bam file name make sense
 
-
-for cramfile in $(find /Volumes/Data/311/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+for pool in 267 311 312 488 564 565 566
 do
-  cramfilename=${cramfile%.*}
-  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
-#  echo "$cramfilename"
-#  echo "$bamfilename"
-  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
-  samtools view -b /Volumes/Data/311/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
-done
-#samtools view -b /Volumes/Data/311/a/LB_311.TI02.cram -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/flight_test/data/F30/LB_311.TI02.bam
-
-for cramfile in $(find /Volumes/Data/312/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
-do
-  cramfilename=${cramfile%.*}
-  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ./bam_name_convert.txt)
-#  echo "$cramfilename"
-#  echo "$bamfilename"
-  rm -r  ~/RS/F30_all/data/${bamfilename}/; mkdir ~/RS/F30_all/data/${bamfilename}/
-  samtools view -b /Volumes/Data/312/a/${cramfile} -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam
+  for cramfile in $(find /Volumes/Data/${pool}/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+  do
+    cramfilename=${cramfile%.*}
+    bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/bam_name_convert.txt)
+  #  echo "$cramfilename"
+  #  echo "$bamfilename"
+    # if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
+    #   mkdir ~/RS/F30_all/data/${bamfilename}/
+    #   samtools view -b -q 20 -F 0x400 -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/311/a/${cramfile}
+    # fi
+  rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
+  samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa \
+  -o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}
+  done | parallel -j 10
 done
