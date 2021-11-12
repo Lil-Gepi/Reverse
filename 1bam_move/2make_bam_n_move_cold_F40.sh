193b32a55e2b8f899febe9b78decdd5c0e995696
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-12 13:57:56 +0100

    cold parallel

--- 1bam_move/2make_bam_n_move_cold_F40.sh
+++ 1bam_move/2make_bam_n_move_cold_F40.sh
@@ -1,17 +1,25 @@
+#!/bin/bash
 
-
-
-for cramfile in $(find /Volumes/Data/185/*/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+for cramfile in $(find /Volumes/Data/185/a/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
 do
   cramfilename=${cramfile%.*}
   bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
-#  echo "$cramfilename"
-#  echo "$bamfilename"
-  # if [ ! -d ~/RS/F30_all/data/${bamfilename}/ ]; then
-  #   mkdir ~/RS/F30_all/data/${bamfilename}/
-  #   samtools view -b -q 20 -F 0x400 -T ~/RS/flight_test/data/reference/dsimM252v1.2+microbiome.fa -o ~/RS/F30_all/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/311/a/${cramfile}
-  # fi
-rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
-samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa \
--o ~/RS/data/${bamfilename}/${bamfilename}.bam /Volumes/Data/${pool}/a/${cramfile}
-done | parallel -j 10
+  rm -r  ~/RS/data/${bamfilename}/; mkdir ~/RS/data/${bamfilename}/
+done
+
+for pool in a b c d e
+do
+  for cramfile in $(find /Volumes/Data/185/${pool}/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+  do
+    cramfilename=${cramfile%.*}
+    bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
+    echo "samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa -o ~/RS/data/${bamfilename}/${bamfilename}_${pool}.bam /Volumes/Data/185/${pool}/${cramfile}"
+  done
+done | parallel -j 8
+
+for cramfile in $(find /Volumes/Data/185/${pool}/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
+do
+  cramfilename=${cramfile%.*}
+  bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
+  samtools merge -o ~/RS/data/${bamfilename}/${bamfilename}.bam ~/RS/data/${bamfilename}/${bamfilename}_a.bam ~/RS/data/${bamfilename}/${bamfilename}_b.bam ~/RS/data/${bamfilename}/${bamfilename}_c.bam ~/RS/data/${bamfilename}/${bamfilename}_d.bam ~/RS/data/${bamfilename}/${bamfilename}_e.bam
+done
