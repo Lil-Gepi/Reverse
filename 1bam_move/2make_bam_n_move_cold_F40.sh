97c0d7620c9a465ee4716f772bd44f0bf52f2a4a
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-16 11:59:51 +0100

    avoid parallel prob cold

--- 1bam_move/2make_bam_n_move_cold_F40.sh
+++ 1bam_move/2make_bam_n_move_cold_F40.sh
@@ -17,7 +17,7 @@ do
     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
     echo "samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa -o ~/RS/data/${bamfilename}/${bamfilename}_${pool}.bam /Volumes/Data/185/${pool}/${cramfile}"
   done
-done | parallel -j 8
+done 
 
 for cramfile in $(find /Volumes/Data/185/${pool}/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
 do
