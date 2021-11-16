301ae8434465c46dd0e9be80604463ab337ee592
Yiwen Chen <brandonchen2932@outlook.com>
2021-11-16 14:25:46 +0100

    fix merge problem

--- 1bam_move/2make_bam_n_move_cold_F40.sh
+++ 1bam_move/2make_bam_n_move_cold_F40.sh
@@ -17,11 +17,11 @@ do
     bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
     echo "samtools view -b -q 20 -F 0x400 -T ~/RS/reference/dsimM252v1.2+microbiome.fa -o ~/RS/data/${bamfilename}/${bamfilename}_${pool}.bam /Volumes/Data/185/${pool}/${cramfile}"
   done
-done 
+done
 
 for cramfile in $(find /Volumes/Data/185/${pool}/ -type f -maxdepth 1 -name "*.cram" -exec basename {} \;)
 do
   cramfilename=${cramfile%.*}
   bamfilename=$(awk -v var="$cramfilename" '$1==var{print $2}' ~/RS/pipeline/1bam_move/cram2bam_name.txt)
-  samtools merge -o ~/RS/data/${bamfilename}/${bamfilename}.bam ~/RS/data/${bamfilename}/${bamfilename}_a.bam ~/RS/data/${bamfilename}/${bamfilename}_b.bam ~/RS/data/${bamfilename}/${bamfilename}_c.bam ~/RS/data/${bamfilename}/${bamfilename}_d.bam ~/RS/data/${bamfilename}/${bamfilename}_e.bam
+  samtools merge ~/RS/data/${bamfilename}/${bamfilename}.bam ~/RS/data/${bamfilename}/${bamfilename}_a.bam ~/RS/data/${bamfilename}/${bamfilename}_b.bam ~/RS/data/${bamfilename}/${bamfilename}_c.bam ~/RS/data/${bamfilename}/${bamfilename}_d.bam ~/RS/data/${bamfilename}/${bamfilename}_e.bam
 done
