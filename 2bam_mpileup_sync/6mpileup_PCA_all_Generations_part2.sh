36b0cf61bcccaee259ce8e2dd02b4fd5c8772d66
Yiwen Chen <brandonchen2932@outlook.com>
2021-12-06 10:57:18 +0100

    modified

--- 2bam_mpileup_sync/6mpileup_PCA_all_Generations_part2.sh
+++ 2bam_mpileup_sync/6mpileup_PCA_all_Generations_part2.sh
@@ -4,9 +4,7 @@ for chr in 2L 2R 3L 3R 4 X
 do
   samtools mpileup -B -q 20 -Q 0 -f ~/RS/reference/dsimM252v1.2+microbiome.fa \
   $(ls -R ~/RS/data/*/*_${chr}.bam) | \
-  # need to figure out how to specifz the Pop and Gen instead of * all bam files.
-  # maybe partition the gen? and R? so that I can put them into the code.
   java -jar ~/RS/pipeline/popoolation2/mpileup2sync.jar --input /dev/stdin \
-  --output ~/RS/result/reverse_${chr}.sync --fastq-type sanger \
+  --output ~/RS/result/grand_PCA_all_generations_${chr}.sync --fastq-type sanger \
   --min-qual 20 --threads 12
 done
