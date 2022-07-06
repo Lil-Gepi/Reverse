2ceb8b96a7bd1762359bb7e8c3f3410b54770b9f
Yiwen Chen <brandonchen2932@outlook.com>
2022-07-06 14:58:18 +0200

    nothing

--- 1cram_bcf/1cram2bcf.operator.sh
+++ 1cram_bcf/1cram2bcf.operator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+working_dir="/Users/ychen/RS"
+cd "${working_dir}/pipeline/"
+
 sample=$1
-id_cram=$(awk -v var="$sample" '$2==var{print $1}' ~/RS/pipeline/1cram_bcf/cram2bam_name.txt)
+id_cram=$(awk -v var="$sample" '$2==var{print $1}' "${working_dir}/pipeline/1cram_bcf/cram2bam_name.txt")
 pool=$(echo ${id_cram:3:3})
 input_cram="/Volumes/Data/${pool}/a/${id_cram}.cram"
 
-working_dir="~/RS"
-cd "${working_dir}/pipeline/"
 out_bcf_dir="${working_dir}/inter_data/${sample}/"
 out_bcf="${out_bcf_dir}/${sample}.bcf"
 ref_fa="${working_dir}/reference/dsimM252v1.2+microbiome.fa"
 
-rl=$(samtools view -u ${input_cram} |\
+echo "now dealing with ${sample}, the data is from pool ${pool}, its id is ${id_cram}"
+
+rl=$(samtools view -T ${ref_fa} -h -F 0x400 ${input_cram} |\
   head -n 1000000 | cut -f 10 | perl -ne 'chomp;print length($_) . "\n"' |\
   sort | uniq -c | sort -n -r | head -n1 |awk -F ' ' '{print $2}')
 
@@ -23,9 +26,10 @@ cov=$(printf "%.0f\n" $( samtools idxstats ${input_cram} | awk -v readlen=${rl} 
         print nreads * readlen / len
     }
 ')| bc -l)
+echo "the read length of ${sample} is ${rl}, and average coverage of ${cov}"
 max_cov=$((cov * 5))
 chroms="X 2L 2R 3L 3R 4"
-samtools view -u ${input_cram} $chroms |\
+samtools view -T ${ref_fa} -h -F 0x400 ${input_cram} $chroms |\
 samtools collate -Ouf - |\
  samtools fixmate -ru - - |\
   samtools view -f 0x2 -h - |\
@@ -33,8 +37,8 @@ samtools collate -Ouf - |\
 ./rupert_pipe_v3/remapq.awk -v NORD=7.5 |\
 samtools sort -@3 -m 4G -u -T /tmp/sortsam.$(basename ${out_bcf%.bcf}).$tag - |\
 bcftools mpileup -f ${ref_fa} -d $max_cov --skip-all-unset 0x2 -q 10 -Q 20 -D -a DP,AD,QS,SCR -Ou --threads 3 - |\
-bcftools annotate -Ov -x INFO/IMF,INFO/VDB,INFO/RPBZ,INFO/MQBZ,INFO/MQSBZ,INFO/SCBZ,INFO/BQBZ,INFO/FS,INFO/SGB,INFO/I16,INFO/QS,INFO/MQ0F,FORMAT/PL - |\
+bcftools annotate -Ov -x INFO/IMF,INFO/VDB,INFO/RPBZ,INFO/MQBZ,INFO/MQSBZ,INFO/SCBZ,INFO/BQBZ,INFO/FS,INFO/SGB,INFO/I16,INFO/QS,INFO/MQ0F,FORMAT/PL |\
 ./rupert_pipe_v3/info2fmt.awk -v tags=DP |\
 bcftools view --no-version -Ob -o "$out_bcf"
-
+echo "done with ${sample}"
 exit
