0b8b1f51b256e2f75288f37ab6e1ad1ad2f9f877
Yiwen Chen <brandonchen2932@outlook.com>
2022-07-20 11:27:42 +0200

    modified for multi sample piling up

--- 2bcf_vcf/1_bcf_single2multi.sh
+++ 2bcf_vcf/1_bcf_single2multi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 in_folder="/Users/ychen/RS/inter_data"
-out_bcf="/Users/ychen/COLD/result/bcf_piled/cold_piled.bcf"
+out_bcf="/Users/ychen/RS/result/piled_bcf/RS_piled.bcf"
 
 for bcf_files in $(ls ${in_folder}/*/*.bcf)
 do
   echo $bcf_files
-done | parallel -j 5 "time bcftools index -f --threads 5 {}"
+done | parallel -j 5 "time bcftools index -f --threads 4 {}"
 
 bcftools merge --no-index --merge both --threads 16 -Ov -o - ${in_folder}/*/*.bcf |\
   /Users/ychen/RS/pipeline/rupert_pipe_v3/post-merging.awk |\
