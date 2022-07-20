6bc0e20c6d195c38f8e644565e4c2200ebf5650f
Yiwen Chen <brandonchen2932@outlook.com>
2022-07-20 11:30:27 +0200

    nothing

--- 2bcf_vcf/1_bcf_single2multi_no_index.sh
+++ 2bcf_vcf/1_bcf_single2multi_no_index.sh
@@ -3,6 +3,6 @@
 in_folder="/Users/ychen/RS/inter_data"
 out_bcf="/Users/ychen/RS/result/piled_bcf/RS_piled.bcf"
 
-bcftools merge --force-samples --merge both --threads 14 -Ov -o - ${in_folder}/*/*.bcf |\
+bcftools merge --force-samples --no-index --merge both --threads 14 -Ov -o - ${in_folder}/*/*.bcf |\
   /Users/ychen/RS/pipeline/rupert_pipe_v3/post-merging.awk |\
   bcftools view --thread 14 --no-version -Ob -o "$out_bcf"
