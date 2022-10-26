b4dc95ed74ad610e1a1831dd6ddfdd533b4021b0
Yiwen Chen <brandonchen2932@outlook.com>
2022-10-26 15:36:14 +0200

    update

--- 2bcf_vcf/1_bcf_single2multi_no_index.sh
+++ 2bcf_vcf/1_bcf_single2multi_no_index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 in_folder="/Users/ychen/RS/inter_data"
-out_bcf="/Users/ychen/RS/result/piled_bcf/RS_piled.bcf"
+out_bcf="/Users/ychen/RS/result/piled_bcf/RS_piled_Oct22.bcf"
 
-bcftools merge --force-samples --no-index --merge both --threads 14 -Ov -o - ${in_folder}/*/*.bcf |\
+bcftools merge --threads 14 --force-samples --no-index --merge both --threads 14 -Ov -o - ${in_folder}/*/*.bcf |\
   /Users/ychen/RS/pipeline/rupert_pipe_v3/post-merging.awk |\
   bcftools view --thread 14 --no-version -Ob -o "$out_bcf"
--- 2bcf_vcf/2_bcf2vcf.sh
+++ 2bcf_vcf/2_bcf2vcf.sh
@@ -3,25 +3,26 @@
 ## 4. Filter mpileup to keep only SNPs
 #
 # input file, can also be "-" if you omit 3.c
-in_bcf="/Users/ychen/COLD/result/bcf_piled/cold_piled.bcf"
+in_bcf="/Users/ychen/RS/result/piled_bcf/RS_piled_Oct22.bcf"
 # BED file of regions to exclude (must match the reference)
 # norepeats_bed="/path/to/norepeats.bed"
 # filter expression removin non-SNPs and positions of dubious
 # coverage depth (adjust as needed; here, ~2200x coverage was
 # expected across all samples in the mpileup)
-flt_expr='TYPE = "snp" & INFO/DP > 1500 & INFO/DP < 7000'
+flt_expr='TYPE = "snp" & INFO/DP > 5000 & INFO/DP < 20000 & sum(FORMAT/SAD) > sum(INFO/DP)*0.5'
 # output path (bgzipped vcf is the most widely used format)
-out_vcf="/Users/ychen/COLD/result/vcf/cold_piled_flt.vcf.gz"
+out_vcf="/Users/ychen/RS/result/vcf/RS_piled_DP_minAF10_SNP_norepeat_Oct22.flt.vcf.gz"
 
 # 4.a filter SNPs close to INDELs or in repeat regions
 # -g 5       ... filter out SNPs that are this close to INDELs
 # -T bedfile ... only output positions described by the provided BED file
 # bcftools filter -g 5 -T "$norepeats_bed" -Ou "$in_bcf" |\
-bcftools filter -g 5 -Ou "$in_bcf" |\
+bcftools filter --threads 5 -g 5 -T "/Users/ychen/COLD/reference/dsimM252v1.2_norepeats+ytranslocs.bed" -Ou "$in_bcf" |\
   # 4.b keep only SNPs and adjust coverage depth thresholds as appropriate
   # (here ~2200x was expected across all samples)
-  bcftools filter -i "$flt_expr" -Ou - | \
+  bcftools filter --threads 5 -i "$flt_expr" -Ou - | \
   # 4.c make positions single-allelic for easier manipulation
-  bcftools norm -m- -Oz - > "$out_vcf"
+  bcftools norm --threads 5 -m- -Ou - | \
+  bcftools view --threads 5 -i 'MAX(FORMAT/AF[:0]) > 0.1' -Oz - > "$out_vcf"
 
 exit
